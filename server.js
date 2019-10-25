b496ba4bb591d55d9a9ecd2ef33f833bb57ccacd
Cristiane Abreu <bjwmcd@yahoo.com>
2019-10-25 06:02:54 -0400

    update

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = 3000;
 
+var app = express()
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
 // Data
 // ===========================================================
 var characters = [
